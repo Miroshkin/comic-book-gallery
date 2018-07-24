70a4b6c77a3244e9a9b90458d7abc3e172990e67
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-07-24 16:10:56 +0300

    Action result method added

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,15 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController: Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from ComicBooksController";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return new RedirectResult("/");
+            }
+
+            return Content("Bla bla bla");
+
         }
     }
 }
